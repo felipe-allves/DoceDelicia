34668e01791f2dea3514b73d443e98dfd040b806
Felipe Alves <felipealves1484@gmail.com>
2025-07-18 19:32:21 -0300

    feat: Header finalizada

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,16 +3,16 @@ import "./header.css";
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="container">
-        <h1 className="logo">Doce Delícia</h1>
-        <nav className="nav-links">
-          <ul>
-            <li>
-              <a href="">Início</a>
-            </li>
+    <>
+      <header>
+        <nav>
+          <div className="logo">Doce Delícia</div>
+          <ul className="nav-links">
             <li>
               <a href="">Produtos</a>
+            </li>
+            <li>
+              <a href="">Especiais</a>
             </li>
             <li>
               <a href="">Sobre</a>
@@ -21,25 +21,25 @@ const Header = () => {
               <a href="">Contato</a>
             </li>
           </ul>
-          <div className="cart-icon" id="cartIcon">
+          <div class="cart-icon" id="cartIcon">
             <svg
               width="24"
               height="24"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              strokeWidth="2"
+              stroke-width="2"
             >
               <path d="M9 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
               <path d="M20 20a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
               <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
             </svg>
-            <span className="cart-count">0</span>
+            <span class="cart-count">0</span>
           </div>
         </nav>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
-export default Header;
+export default Header
