9f44fc236f0a86b7126827ee310bf0a39a7d37ea
Felipe Alves <felipealves1484@gmail.com>
2025-07-19 18:20:37 -0300

    fix: corrigindo imports case-sensitive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-import Header from './components/header/Header'
-import Hero from './components/hero/Hero'
-import Benefits from './components/benefits/Benefits'
-import Products from './components/Products/products'
-import Specialties from './components/specialties/Specialties'
-import History from './components/history/History'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import Benefits from './components/Benefits/Benefits'
+import Products from './components/Products/Products'
+import Specialties from './components/Specialties/Specialties'
+import History from './components/History/History'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
